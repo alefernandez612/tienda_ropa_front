fecc102e26a4c081af45d55fcd5a489f80959932
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-09 14:16:54 +0200

    Creado componente Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {AppComponent} from './app.component';
 import {LoginComponent} from './components/login/login.component';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
