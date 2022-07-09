fab3e8c46f2815602f211c0407c2a5b81ffa43a8
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-09 14:16:54 +0200

    Revert "Creado componente Home"
    This reverts commit fecc102e26a4c081af45d55fcd5a489f80959932.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: '/home'},
-  {path: 'home', component: HomeComponent},
-  {path: '**', pathMatch: 'full', redirectTo: '/home'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
